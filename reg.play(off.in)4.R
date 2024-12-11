631faa7cbf20f7ade74eaf9ab8cf71b5dbf44595
Levi Diamond <levi.diamond1@marist.edu>
2024-12-11 15:44:20 -0500

    Add files via upload

---
+++ reg.play(off.in)4.R
@@ -0,0 +1,51 @@
+NBAstats23.GP$year <- 2023
+NBAstats22.GP$year <- 2022
+NBAstats21.GP$year <- 2021
+NBAstats20.GP$year <- 2020
+NBAstats19.GP$year <- 2019
+NBAstats18.GP$year <- 2018
+NBAstats17.GP$year <- 2017
+NBAstats16.GP$year <- 2016
+NBAstats15.GP$year <- 2015
+NBAstats14.GP$year <- 2014
+NBAstats13.GP$year <- 2013
+NBAstats12.GP$year <- 2012
+NBAstats11.GP$year <- 2011
+NBAstats10.GP$year <- 2010
+NBAstats09.GP$year <- 2009
+NBAstat08.GP$year <- 2008
+NBAstat07.GP$year <- 2007
+NBAstat06.GP$year <- 2006
+NBAstat05.GP$year <- 2005
+NBAstat04.GP$year <- 2004
+NBAstat03.GP$year <- 2003
+NBAstat02.GP$year <- 2002
+NBAstat01.GP$year <- 2001
+NBAstat00.GP$year <- 2000
+NBAstat99.GP$year <- 1999
+NBAstat98.GP$year <- 1998
+NBAstat97.GP$year <- 1997
+NBAstat96.GP$year <- 1996
+
+
+NBAstat08.GP -> NBAstats08.GP
+NBAstat07.GP -> NBAstats07.GP
+NBAstat06.GP -> NBAstats06.GP
+NBAstat05.GP -> NBAstats05.GP
+NBAstat04.GP -> NBAstats04.GP
+NBAstat03.GP -> NBAstats03.GP
+NBAstat02.GP -> NBAstats02.GP
+NBAstat01.GP -> NBAstats01.GP
+NBAstat00.GP -> NBAstats00.GP
+NBAstat99.GP -> NBAstats99.GP
+NBAstat98.GP -> NBAstats98.GP
+NBAstat97.GP -> NBAstats97.GP
+NBAstat96.GP -> NBAstats96.GP
+
+NBAstats.GP <- bind_rows(NBAstats96.GP, NBAstats97.GP, NBAstats98.GP, NBAstats99.GP , NBAstats00.GP, NBAstats01.GP, NBAstats02.GP, NBAstats03.GP, NBAstats04.GP, NBAstats05.GP,NBAstats06.GP, NBAstats07.GP, NBAstats08.GP, NBAstats09.GP, NBAstats10.GP, NBAstats11.GP, NBAstats12.GP, NBAstats13.GP, NBAstats14.GP, NBAstats15.GP, NBAstats16.GP, NBAstats17.GP, NBAstats18.GP, NBAstats19.GP, NBAstats20.GP, NBAstats21.GP, NBAstats22.GP, NBAstats23.GP)
+
+View(NBAstats.GP)
+
+
+
+
